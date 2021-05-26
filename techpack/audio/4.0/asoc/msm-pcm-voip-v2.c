5208f405c6f7a359f548701d11498f60a0e50066
Nick C <pokemonnekitgod@gmail.com>
2021-05-26 16:45:48 +0200

    techpack/audio: silence
    'UL data dropped' while whatsapp video call.

--- techpack/audio/4.0/asoc/msm-pcm-voip-v2.c
+++ techpack/audio/4.0/asoc/msm-pcm-voip-v2.c
@@ -495,7 +495,6 @@ static void voip_process_ul_pkt(uint8_t *voc_pkt,
 		snd_pcm_period_elapsed(prtd->capture_substream);
 	} else {
 		spin_unlock_irqrestore(&prtd->dsp_ul_lock, dsp_flags);
-		pr_err("UL data dropped\n");
 	}
 
 	wake_up(&prtd->out_wait);
